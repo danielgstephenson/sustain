ec9649ad95dd80e7a3cc3f9eb8cfd62ada4e3741
dgstephenson <dgstephenson.econ@gmail.com>
2022-08-30 20:34:45 -0400

    plant

--- public/client.js
+++ public/client.js
@@ -5,16 +5,20 @@ const context1 = canvas1.getContext('2d')
 
 const N = 8
 const L = N - 1
+const tickInterval = 100
+const year = 20
+const tilt = 2 * Math.random() - 1
+
 const canvas0 = new OffscreenCanvas(N, N)
 const context0 = canvas0.getContext('2d')
 context0.imageSmoothingEnabled = false
 
-// Disable Right Click Menu
-document.oncontextmenu = () => false
-
 function range (n) { return [...Array(n).keys()] }
 function clamp (a, b, x) { return Math.max(a, Math.min(b, x)) }
 // const sum = array => array.reduce((a, b) => a + b, 0)
+
+// Disable Right Click Menu
+document.oncontextmenu = () => false
 
 const mouse = {
   down: [false, false, false],
@@ -26,10 +30,14 @@ const mouse = {
 const keys = {
   shift: false
 }
-let scale = 1
+const state = {
+  time: 0,
+  scale: 1
+}
 
 const grid = range(N).map(i => range(N).map(j => {
-  const node = { color: 'green', fill: Math.random(), growth: 0, x: j, y: i }
+  const node = { color: 'green', fill: 0.5, growth: 0, x: j, y: i }
+  node.season = 0
   node.edges = []
   node.neighbors = []
   return node
@@ -41,10 +49,6 @@ range(N).forEach(i => range(N).forEach(j => {
   if (i > 0) node.neighbors.push(grid[i - 1][j])
   if (j < L) node.neighbors.push(grid[i][j + 1])
   if (j > 0) node.neighbors.push(grid[i][j - 1])
-  if (i < L && j < L) node.neighbors.push(grid[i + 1][j + 1])
-  if (i < L && j > 0) node.neighbors.push(grid[i + 1][j - 1])
-  if (i > 0 && j < L) node.neighbors.push(grid[i - 1][j + 1])
-  if (i > 0 && j > 0) node.neighbors.push(grid[i - 1][j - 1])
 }))
 const edges = []
 range(N).forEach(i => range(N).forEach(j => {
@@ -70,19 +74,19 @@ console.log(edges)
 function updateMouse (e) {
   const cx = canvas1.getBoundingClientRect().left
   const cy = canvas1.getBoundingClientRect().top
-  mouse.y = Math.floor((e.pageY - cy) * N / scale)
-  mouse.x = Math.floor((e.pageX - cx) * N / scale)
+  mouse.y = Math.floor((e.pageY - cy) * N / state.scale)
+  mouse.x = Math.floor((e.pageX - cx) * N / state.scale)
   if (mouse.x >= 0 && mouse.x < N && mouse.y >= 0 && mouse.y < N) {
     mouse.node = grid[mouse.y][mouse.x]
   }
 }
 
 function plant () {
-  if (mouse.down[0] && mouse.node.state === 'empty') {
+  if (mouse.down[0]) {
     mouse.node.state = 'blue'
   }
-  if (mouse.down[2] && mouse.node.state === 'empty') {
-    mouse.node.state = 'green'
+  if (mouse.down[2]) {
+    mouse.node.state = 'red'
   }
 }
 
@@ -96,9 +100,16 @@ window.onmousedown = function (e) {
   if (e.button === 1) mouse.down[1] = true
   if (e.button === 2) mouse.down[2] = true
   updateMouse(e)
-  if (e.button === 0) mouse.node.color = 'blue'
-  if (e.button === 2) mouse.node.color = 'green'
   console.log(mouse.node)
+  const oldColor = mouse.node.color
+  if (e.button === 0) {
+    if (oldColor === 'green') mouse.node.color = 'blue'
+    if (oldColor === 'blue') mouse.node.color = 'green'
+  }
+  if (e.button === 2) {
+    if (oldColor === 'green') mouse.node.color = 'red'
+    if (oldColor === 'red') mouse.node.color = 'green'
+  }
 }
 
 window.onmouseup = function (e) {
@@ -116,21 +127,36 @@ window.onkeyup = function (e) {
 }
 
 function update () {
-  const dt = 0.01
-  edges.forEach(edge => {
-    edge.flow += dt * (edge.a.fill - edge.b.fill)
-    edge.flow = clamp(-0.2, 0.2, edge.flow)
-  })
-  edges.forEach(edge => {
-    const flow = clamp(-0.2 * edge.b.fill, 0.2 * edge.a.fill, edge.flow)
-    edge.a.fill -= flow
-    edge.b.fill += flow
+  const dt = tickInterval / 1000
+  state.time += dt
+  range(30).forEach(i => {
+    edges.forEach(edge => {
+      const maxFlow = 0.25 * edge.a.fill
+      const minFlow = -0.25 * edge.b.fill
+      edge.flow += 0.1 * dt * (edge.a.fill - edge.b.fill)
+      if (edge.a.color === edge.b.color) {
+        edge.flow = clamp(minFlow, maxFlow, edge.flow)
+      } else if (edge.a.color === 'green') {
+        edge.flow = clamp(0, maxFlow, edge.flow)
+      } else if (edge.b.color === 'green') {
+        edge.flow = clamp(minFlow, 0, edge.flow)
+      } else {
+        edge.flow = 0
+      }
+    })
+    edges.forEach(edge => {
+      edge.a.fill -= edge.flow
+      edge.b.fill += edge.flow
+    })
   })
   nodes.forEach(node => {
     if (node.color === 'green') {
-      node.growth = dt * (0.01 + node.fill)
+      const nodeTime = 2 * state.time / year + 0.1 * node.y / N
+      node.season = Math.sin(2 * Math.PI * nodeTime)
+      node.growth = dt * 0.5
     } else {
-      node.growth = -dt
+      node.growth = -dt * 0.5
+      node.capacity = 1
     }
     node.fill = clamp(0, 1, node.fill + node.growth)
     if (node.fill < 0.1) node.color = 'green'
@@ -138,13 +164,13 @@ function update () {
 }
 
 function setupCanvas () {
-  scale = 0.95 * Math.min(window.innerHeight, window.innerWidth)
-  canvas1.width = scale
-  canvas1.height = scale
+  state.scale = 0.95 * Math.min(window.innerHeight, window.innerWidth)
+  canvas1.width = state.scale
+  canvas1.height = state.scale
   const xTranslate = 0
   const yTranslate = 0
-  const xScale = scale / N
-  const yScale = scale / N
+  const xScale = state.scale / N
+  const yScale = state.scale / N
   context1.setTransform(xScale, 0, 0, yScale, xTranslate, yTranslate)
   context1.imageSmoothingEnabled = false
 }
@@ -173,4 +199,5 @@ function draw () {
 }
 
 draw()
-setInterval(update, 20)
+
+setInterval(update, tickInterval)
