bd023da6fff787a88b5a1052ab3032b58af365f3
dgstephenson <dgstephenson.econ@gmail.com>
2024-04-13 22:48:31 -0400

    svg

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { makeIo } from './server.js'
 
+console.log('Hello server.')
+
+const serverId = Math.random()
+const players = new Map()
+const sockets = new Map()
+const updateInterval = 0.1
+const mapRadius = 5
+
 const io = makeIo(() => {
   setInterval(update, updateInterval * 1000)
 })
@@ -20,6 +28,8 @@ io.on('connection', socket => {
   socket.on('clientUpdateServer', message => {
     const reply = {
       team: player.team,
+      serverId,
+      mapRadius,
       nodes
     }
     socket.emit('updateClient', reply)
@@ -40,10 +50,18 @@ function choose (array) {
 // function sum (array) { return array.reduce((a, b) => a + b, 0) }
 // function clamp (a, b, x) { return Math.max(a, Math.min(b, x)) }
 
-const players = new Map()
-const sockets = new Map()
-const updateInterval = 0.1
-const mapRadius = 2
+const Q = {
+  x: 1,
+  y: 0
+}
+const R = {
+  x: -0.5,
+  y: Math.sqrt(3 / 4)
+}
+const S = {
+  x: -0.5,
+  y: -Math.sqrt(3 / 4)
+}
 
 const nodes = makeNodes()
 
@@ -57,41 +75,61 @@ function update () {
 
 function makeNodes () {
   const nodes = []
+  const nodeMap = []
   const length = 1 + 2 * mapRadius
   range(length).forEach(i => {
+    nodeMap[i] = []
+    const q = i - mapRadius
     range(length).forEach(j => {
-      range(length).forEach(k => {
-        const q = i - mapRadius
-        const r = j - mapRadius
-        const s = k - mapRadius
-        if (q + r + s === 0) {
-          nodes.push(makeNode(q, r, s))
+      const r = j - mapRadius
+      const s = 0 - q - r
+      const inRange = -mapRadius <= s && s <= mapRadius
+      const open = Math.random() < 1 & (q !== 0 | r !== 0)
+      if (inRange && open) {
+        const node = makeNode(q, r, s, nodes.length)
+        nodes.push(node)
+        nodeMap[i][j] = node
+      }
+    })
+  })
+  range(length).forEach(i => {
+    range(length).forEach(j => {
+      const node = nodeMap[i][j]
+      if (node) {
+        const neighbors = [
+          nodeMap[i][j + 1],
+          nodeMap[i][j - 1]
+        ]
+        if (nodeMap[i + 1]) {
+          neighbors.push(...[
+            nodeMap[i + 1][j],
+            nodeMap[i + 1][j - 1]
+          ])
         }
-      })
+        if (nodeMap[i - 1]) {
+          neighbors.push(...[
+            nodeMap[i - 1][j],
+            nodeMap[i - 1][j + 1]
+          ])
+        }
+        neighbors.forEach(neighbor => {
+          if (neighbor) node.neighbors.push(neighbor.index)
+        })
+      }
     })
   })
   return nodes
 }
 
-function makeNode (q, r, s) {
-  const Q = {
-    x: 1,
-    y: 0
-  }
-  const R = {
-    x: -0.5,
-    y: Math.sqrt(3 / 4)
-  }
-  const S = {
-    x: -0.5,
-    y: -Math.sqrt(3 / 4)
-  }
+function makeNode (q, r, s, index) {
   const node = {
-    align: choose([0, 1, 2]),
+    align: choose([0, 1, 2, 3]),
+    q,
+    r,
+    index,
     x: q * Q.x + r * R.x + s * S.x,
     y: q * Q.y + r * R.y + s * S.y,
-    index: 0,
-    life: 0.3,
+    life: Math.random(),
     neighbors: []
   }
   return node
--- public/client.js
+++ public/client.js
@@ -1,91 +1,78 @@
 import { io } from './socketIo/socket.io.esm.min.js'
-const socket = io()
-
-const canvas = document.getElementById('mapCanvas')
-const context = canvas.getContext('2d')
-const colors = {
-  0: { H: 60, S: 100, L: 40 },
-  1: { H: 240, S: 100, L: 50 },
-  2: { H: 130, S: 100, L: 35 }
-}
-
-let msgLog = {}
-let nodes = []
-
-socket.on('updateClient', (msg) => {
-  // const cursor = msg.team === 1 ? "url('BlueCursor.png') 1 1, pointer" : "url('GreenCursor.png') 1 1, pointer"
-  // document.body.style.cursor = cursor
-  msgLog = msg
-  nodes = msg.nodes
-})
+const svgns = 'http://www.w3.org/2000/svg'
+const mapSvg = document.getElementById('mapSvg')
 
 function range (n) { return [...Array(n).keys()] }
 // function clamp (a, b, x) { return Math.max(a, Math.min(b, x)) }
 // const sum = array => array.reduce((a, b) => a + b, 0)
 
+let msgLog = {}
+let nodes = []
+let hexes = []
+let serverId = 0
+let mapRadius = 1
+
+const socket = io()
+socket.on('updateClient', (msg) => {
+  msgLog = msg
+  nodes = msg.nodes
+  if (msg.serverId !== serverId) {
+    mapRadius = msg.mapRadius
+    serverId = msg.serverId
+    setupMap()
+  }
+  updateHexColors()
+})
+
 window.oncontextmenu = () => false
 window.onmousedown = () => {
+  console.log(serverId)
   console.log(msgLog)
+  console.log(nodes)
 }
 
-function setupCanvas () {
-  const vmin = Math.min(window.innerHeight, window.innerWidth)
-  canvas.width = vmin
-  canvas.height = vmin
-  resetContext()
-}
+const radius = 0.95
+let hexPoints = `${radius},0`
+range(6).forEach(i => {
+  const angle = 2 * Math.PI * (i + 1) / 6
+  const x = Math.cos(angle) * radius
+  const y = Math.sin(angle) * radius
+  hexPoints = hexPoints + ` ${x},${y}`
+})
 
-function resetContext () {
-  const vmin = Math.min(window.innerHeight, window.innerWidth)
-  context.resetTransform()
-  context.translate(0.5 * vmin, 0.5 * vmin)
-  context.scale(0.1 * vmin, -0.1 * vmin)
-}
-
-function drawNodes () {
-  context.clearRect(0, 0, 100, 100)
+function setupMap () {
+  mapSvg.innerHTML = ''
+  hexes = []
+  const z = 2 + 2 * Math.sqrt(3 / 4) * mapRadius
+  mapSvg.setAttributeNS(null, 'viewBox', `-${z} -${z} ${2 * z} ${2 * z}`)
   nodes.forEach(node => {
-    const size = 1
-    const color = colors[node.align]
-    resetContext()
-    context.fillStyle = `hsla(${color.H}, ${color.S}%, ${color.L}%, 0.3)`
-    context.translate(size * node.x, size * node.y)
-    context.beginPath()
-    context.moveTo(size, 0)
-    range(6).forEach(i => {
-      const angle = 2 * Math.PI * (i + 1) / 6
-      const x = size * Math.cos(angle)
-      const y = size * Math.sin(angle)
-      context.lineTo(x, y)
-    })
-    context.fill()
-    resetContext()
-    const radius = Math.sqrt(node.life)
-    context.fillStyle = `hsla(${color.H}, ${color.S}%, ${color.L}%, 1)`
-    context.translate(size * node.x, size * node.y)
-    context.beginPath()
-    context.moveTo(size * radius, 0)
-    range(6).forEach(i => {
-      const angle = 2 * Math.PI * (i + 1) / 6
-      const x = size * Math.cos(angle) * radius
-      const y = size * Math.sin(angle) * radius
-      context.lineTo(x, y)
-    })
-    context.fill()
+    const hex = document.createElementNS(svgns, 'polygon')
+    hex.setAttributeNS(null, 'points', hexPoints)
+    hex.setAttributeNS(null, 'transform', `translate(${node.x},${node.y})`)
+    mapSvg.appendChild(hex)
+    hexes[node.id] = hex
   })
 }
 
-function draw () {
-  setupCanvas()
-  drawNodes()
-  window.requestAnimationFrame(draw)
+const colors = {
+  0: { H: 120, S: 100, L: 40 },
+  1: { H: 240, S: 100, L: 50 },
+  2: { H: 0, S: 100, L: 40 },
+  3: { H: 0, S: 0, L: 70 }
 }
 
-draw()
-
-setInterval(updateServer, 100)
+function updateHexColors () {
+  hexes.forEach((hex, i) => {
+    const node = nodes[i]
+    const color = colors[node.align]
+    const fill = `hsla(${color.H}, ${color.S}%, ${color.L}%, ${0.25 + 0.75 * node.life})`
+    hex.setAttributeNS(null, 'style', `fill: ${fill}; stroke: none; stroke-width: 0;`)
+  })
+}
 
 function updateServer () {
   const msg = { }
   socket.emit('clientUpdateServer', msg)
 }
+
+setInterval(updateServer, 100)
